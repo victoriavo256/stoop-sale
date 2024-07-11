f187d9b91494e9a9cc9325e64895ac30e8e65f5c
victoriavo256 <victoria.vo256@gmail.com>
2024-07-11 13:57:05 -0700

    css animations for opening envelope

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // when "closed envelope" image is clicked, "open envelope" image is revealed 
 // and text + images pop out to fill the screen with an invite to the stoop sale
+
 envelopeButton = document.getElementById("closed-envelope");
+
 envelopeButton.addEventListener("click", () => {
+
     // hide closed envelope
     envelopeButton.classList.add('hidden');
+
     // show opened envelope
     document.getElementById("opened-envelope").classList.remove('hidden');
-    // show other text + images popping out from envelope
+
+    setTimeout(() => {
+        // after a 1.1s delay, hide opened envelope with a fadeaway animation
+        document.getElementById("opened-envelope").classList.add('hide');
+    }, 1100);
+
+    setTimeout(() => {
+        // after waiting 1s for the fadeaway animation to finish playing out, 
+        // officially hide the opened envelope and...
+        document.getElementById("opened-envelope").classList.add('hidden');
+
+        // ...show other text + images popping out from where the envelope was
+        document.getElementById("pop-out-from-envelope").classList.remove('hidden')
+    }, 1000)
 });
